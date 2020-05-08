eec143953aa4cef16dc5512acf88bbc53e0ecaa5
rupesh1310 <a.rupesh66@gmail.com>
2020-05-08 12:22:16 +0530

    :tada: added directory state into redux & collection state into Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,21 @@
 import React from "react";
-
-import SHOP_DATA from "./shop.data.js";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from "../../redux/shop/shop.selectors";
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...othersCollectionProps }) => (
-          <CollectionPreview key={id} {...othersCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...othersCollectionProps }) => (
+      <CollectionPreview key={id} {...othersCollectionProps} />
+    ))}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
