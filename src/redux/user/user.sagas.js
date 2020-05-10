e06393b117f4bef24978cd61e66e978858ea7e3a
rupesh1310 <a.rupesh66@gmail.com>
2020-05-11 03:07:44 +0530

    :rocket: email signIn into sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,8 +1,11 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
-
 import UserActionTypes from "./user.types";
-
-import { googleSignInSuccess, googleSignInFailure } from "./user.actions";
+import {
+  googleSignInSuccess,
+  googleSignInFailure,
+  emailSignInSuccess,
+  emailSignInFailure,
+} from "./user.actions";
 
 import {
   auth,
@@ -23,10 +26,27 @@ export function* signInWithGoogle() {
   }
 }
 
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapShot = yield userRef.get();
+    yield put(
+      emailSignInSuccess({ id: userSnapShot.id, ...userSnapShot.data() })
+    );
+  } catch (error) {
+    yield put(emailSignInFailure(error));
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* onEmailSignInStart() {
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart)]);
+  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
