649cc603324e597510511a7bf32e4eb005d74461
rupesh1310 <a.rupesh66@gmail.com>
2020-05-13 04:25:16 +0530

    :rocket: :art: added react lazy and optimized a bit

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,8 @@
-import React from 'react';
+import React from "react";
+import Spinner from "../spinner/spinner.component";
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
-
-const WithSpinner = WrappedComponent => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-  return Spinner;
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
